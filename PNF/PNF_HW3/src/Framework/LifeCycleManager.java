4e0da4dea02de326367df8b76dbdddcecb5bf7bc
chaerlo127 <monynony0203@gmail.com>
2022-12-01 00:40:03 +0900

    create event-bus project

--- PNF/PNF_HW3/src/Framework/LifeCycleManager.java
+++ PNF/PNF_HW3/src/Framework/LifeCycleManager.java
@@ -32,7 +32,7 @@ public class LifeCycleManager {
             
             thread1.start();
             thread2.start();
-            thread3.start();
+            thread3.start();  
             thread4.start();
             thread5.start();
         } catch(Exception e) {
